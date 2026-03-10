7228aa78bdb14101ea9becf79645fb59aa9df501
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2026-03-10 00:35:43 -0300

    Create Resultandwriteint.php to compute division
    Add PHP script to compute and display the result of dividing two numbers.

---
+++ PHP/Resultandwriteint/Resultandwriteint.php
@@ -0,0 +1,10 @@
+//Compiler result two numbers and print one message "Result the two numbers"
+
+<?php
+//Variables 
+$intNum =8/2;
+$stringWrite="Result the two numbers!";
+
+//Print result
+echo $intNum;
+echo "\n",$stringWrite;
