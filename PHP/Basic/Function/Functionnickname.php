78d226f9e9130289ca2cbf67f66f64b852341715
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2026-03-17 23:47:22 -0300

    Create Functionnickname.php

---
+++ PHP/Basic/Function/Functionnickname.php
@@ -0,0 +1,13 @@
+//Create function about one print nickname with return.
+
+<?php
+
+//Function
+function nickname($name){
+
+    //Print
+    echo "Hello my nickname is:".$name;
+}
+//Return print
+nickname("Tassi");
+?>
